2bf856cc6adc3443b4addfe9af8c0233f307383f
markkyn <marcos.gabriel@ossirian.com>
2024-03-26 18:58:31 -0300

    Alguns modelos ORM

--- src/common/models.py
+++ src/common/models.py
@@ -1,1 +1,47 @@
 from django.db import models
+
+class Especialização(models.Model):
+    nome = models.CharField(
+        max_lenght = 64,
+        required = True,
+        unique = True
+    )
+
+class ProfissionalSaude(models.Model):
+    nome = models.CharField(
+        max_lenght = 64,
+        required = True,
+    )
+
+    cpf = models.CharField(
+        max_lenght = 11,
+        required = True,
+        unique = True
+    )
+
+class Medico(models.Model):
+    crm = models.CharField(
+        max_length = 12,
+        primary_key = True,
+        unique = True,
+        required = True,
+
+    )
+    
+    profissional = models.ForeignKey(
+        ProfissionalSaude,
+    )
+    
+class Enfermeiro(models.Model):
+    coren = models.CharField(
+        max_lenght = 12,
+        primary_key = True,
+        required = True,
+        unique = True
+    )
+
+    profissional = models.ForeignKey(
+        ProfissionalSaude,
+    )
+
+
